b69535b83cac61c47c4e3f1e119e0740485f8e0f
JericHunter <jeric.hunterr@gmail.com>
2023-12-14 03:24:21 -0500

    update_ratings func
    adding the update ratings function to update the ratings of players based on the match outcome

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ def calculate_expected_outcome(player_a_rating, player_b_rating):
     """
     exponent = (player_b_rating - player_a_rating) / 400.0
     return 1 / (1 + math.pow(10, exponent))
+
+def update_ratings(winner, loser, k_factor=32):
+    """
+    Update the ratings of players based on the match outcome.
+    """
+    expected_outcome_winner = calculate_expected_outcome(winner.rating, loser.rating)
+    expected_outcome_loser = 1 - expected_outcome_winner
+
+    # Calculate rating changes
+    rating_change_winner = k_factor * (1 - expected_outcome_winner)
+    rating_change_loser = k_factor * (0 - expected_outcome_loser)
+
+    # Update ratings
+    winner.rating += rating_change_winner
+    loser.rating += rating_change_loser
