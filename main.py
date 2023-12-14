44eab3ee9ae37353616add0de20cfdcbb2dc514b
JericHunter <jeric.hunterr@gmail.com>
2023-12-14 02:53:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Player:
-    def __init__(self, player_id, rating=1000):
+    def __init__(self, player_id, rating=1000, username=None):
         self.player_id = player_id
         self.rating = rating
+        self.username = username
 
