94119013ed60256a4a0a3d1c7b330f71b1add960
JericHunter <jeric.hunterr@gmail.com>
2023-12-14 02:56:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import math
 class Player:
     def __init__(self, player_id, rating=1000, username=None):
         self.player_id = player_id
         self.rating = rating
         self.username = username
 
+def calculate_expected_outcome(player_a_rating, player_b_rating):
+    """
+    Calculate the expected outcome of a match between two players.
+    """
+    exponent = (player_b_rating - player_a_rating) / 400.0
+    return 1 / (1 + math.pow(10, exponent))
